b21ab7e7d8365b7674b3c569fdd53c041fcf24fa
Nick Conklin <nconkl1@outlook.com>
2019-02-28 09:40:43 -0500

    Update events.py

--- events.py
+++ events.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import constants
-import sqlite3
+import asyncpg
 
 
 class EventsCog(commands.Cog):
@@ -22,31 +22,33 @@ class EventsCog(commands.Cog):
         await member.send(embed=embed)
 
         try:
-            self.bot.db.set_default_coins(member.id, member.name, coins=1000)
+            await self.bot.db.set_default_coins(member.id, member.name, coins=1000)
             await bot_spam_channel.send(
                 f"{constants.success_string} Member ({member.id}) joined the server!  1000 coins "
                 f"have been added.")
-        except sqlite3.Error as e:
-            print(e)
+        except asyncpg.UniqueViolationError:
             await bot_spam_channel.send(
                 f"{constants.error_string} Member ({member.id}) already exists.  Coins not added.")
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
-        channel = self.bot.get_channel(541969928606056462)
-        self.bot.db.remove_user_from_db(member.id)
-        await channel.send(f"{constants.success_string} {member.name} left the server.  Removed coins.")
+        bot_spam_channel = self.bot.get_channel(541969928606056462)
+        await self.bot.db.remove_user_from_db(member.id)
+        await bot_spam_channel.send(f"{constants.success_string} {member.name} left the server.  Removed coins.")
 
     @commands.Cog.listener()
-    # TODO: Don't print stuff every time the bot loads and don't update db every time
     async def on_ready(self):
+        await self.bot.db.start_connection()
         print(f"\n\nLogged in as: {self.bot.user.name} - {self.bot.user.id}\nVersion: {discord.__version__}\n")
         await self.bot.change_presence(activity=discord.Game(name='Use $help'))
         print(f"Successfully logged in and booted...!")
         guild_members = self.bot.get_all_members()
         for member in guild_members:
             if not member.bot:
-                self.bot.db.set_default_coins(member.id, member.name, coins=1000)
+                try:
+                    await self.bot.db.set_default_coins(member.id, member.name, coins=1000)
+                except asyncpg.UniqueViolationError:
+                    print(f"User {member.name} already exists in database...skipping.")
 
 
 def setup(bot):
